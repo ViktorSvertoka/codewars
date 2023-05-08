c0213137b46420e01e32fd103796fc736d6a877b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-09 00:21:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,33 @@ function positiveSum(arr) {
 
 console.log(positiveSum([1, -4, 7, 12]));
 console.log(positiveSum([-1, -4, -7, -12]));
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0) {
+    return [];
+  }
+
+  let countPositives = 0;
+  let sumNegatives = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      countPositives++;
+    } else if (input[i] < 0) {
+      sumNegatives += input[i];
+    }
+  }
+
+  return [countPositives, sumNegatives];
+}
