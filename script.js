759bc7151d4988453b2de00408c59c53db94b44c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-13 23:05:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -126,3 +126,32 @@ function countPositivesSumNegatives(input) {
 
   return [countPositives, sumNegatives];
 }
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+function past(h, m, s) {
+  let milliseconds = 0;
+
+  milliseconds += h * 60 * 60 * 1000;
+  milliseconds += m * 60 * 1000;
+  milliseconds += s * 1000;
+  return milliseconds;
+}
+
+console.log(past(0, 1, 1));
