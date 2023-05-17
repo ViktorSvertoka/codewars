dbb1df6e88aebdf3054817fcec02ea5de6245a3b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-17 22:56:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -239,3 +239,23 @@ class Kata {
 console.log(Kata.getVolumeOfCuboid(2, 3, 4)); // 24
 console.log(Kata.getVolumeOfCuboid(1, 1, 1)); // 1
 console.log(Kata.getVolumeOfCuboid(5, 5, 5)); // 125
+
+//----------------------------------------------------------------------------------------------------------------
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+  return num.toString();
+}
+
+// Примеры использования
+console.log(numberToString(123)); // Output: "123"
+console.log(numberToString(999)); // Output: "999"
+console.log(numberToString(-100)); // Output: "-100"
