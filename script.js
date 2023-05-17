dcd0d92dcd28d6acde53a129651f83c2388811e5
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-17 23:02:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -298,3 +298,41 @@ console.log(getSum(0, 1)); // Output: 1
 console.log(getSum(1, 1)); // Output: 1
 console.log(getSum(-1, 0)); // Output: -1
 console.log(getSum(-1, 2)); // Output: 2
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+function getMiddle(s) {
+  const length = s.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length % 2 === 0) {
+    return s.substring(middleIndex - 1, middleIndex + 1);
+  } else {
+    return s.charAt(middleIndex);
+  }
+}
+
+// Примеры использования
+console.log(getMiddle('test')); // Output: "es"
+console.log(getMiddle('testing')); // Output: "t"
+console.log(getMiddle('middle')); // Output: "dd"
+console.log(getMiddle('A')); // Output: "A"
