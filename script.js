b90fe20461358f9a8a437e367e6bae3111dfa303
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-19 01:56:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -336,3 +336,24 @@ console.log(getMiddle('test')); // Output: "es"
 console.log(getMiddle('testing')); // Output: "t"
 console.log(getMiddle('middle')); // Output: "dd"
 console.log(getMiddle('A')); // Output: "A"
+
+//---------------------------------------------------------------------------------------------------------------
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr(n, s) {
+  let result = '';
+
+  for (let i = 0; i < n; i++) {
+    result += s;
+  }
+
+  return result;
+}
+
+console.log(repeatStr(6, 'I')); // Вывод: "IIIIII"
+console.log(repeatStr(5, 'Hello')); // Вывод: "HelloHelloHelloHelloHello"
