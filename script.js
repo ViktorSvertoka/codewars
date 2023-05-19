2fb414e83dc2a11ede885345adb6bd210fe08d11
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-19 10:36:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -357,3 +357,24 @@ function repeatStr(n, s) {
 
 console.log(repeatStr(6, 'I')); // Вывод: "IIIIII"
 console.log(repeatStr(5, 'Hello')); // Вывод: "HelloHelloHelloHelloHello"
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+function addBinary(a, b) {
+  const sum = a + b;
+  return sum.toString(2);
+}
+
+console.log(addBinary(1, 1)); // Output: "10"
+console.log(addBinary(5, 9)); // Output: "1110"
+
+//-------------------------------------------------------------------------------------------------------------------
