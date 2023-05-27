a519b2291d904fdd3cc3088aa01ce8b517cc36ca
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-28 00:05:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -554,3 +554,30 @@ const arrayOfSheep = [
 ];
 const sheepCount = countSheeps(arrayOfSheep);
 console.log(sheepCount); // Output: 17
+
+//---------------------------------------------------------------------------------------------------------------
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+// Testing the function
+console.log(makeNegative(1)); // Output: -1
+console.log(makeNegative(-5)); // Output: -5
+console.log(makeNegative(0)); // Output: 0
+console.log(makeNegative(0.12)); // Output: -0.12
