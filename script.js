64e057619320036a5b086cc5840f5527f700db53
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-29 22:41:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -648,3 +648,19 @@ var summation = function (num) {
 };
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+console.log(stringToArray('Robin Singh')); // Output: ["Robin", "Singh"]
+console.log(stringToArray('I love arrays they are my favorite')); // Output: ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//---------------------------------------------------------------------------------------------------------------
