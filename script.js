a8381d99c334bcbe00023a7d66fdfe161f9cae23
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-04 01:04:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -810,3 +810,25 @@ const areaOrPerimeter = function (l, w) {
 };
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(5, 10, 2)); // Output: 17
+
+//---------------------------------------------------------------------------------------------------------------
