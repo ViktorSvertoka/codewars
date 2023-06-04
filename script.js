70664e0acb7ace00947c5c0c3d89d7d5699e777e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-05 00:06:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -861,4 +861,5 @@ function accum(s) {
 console.log(accum('abcd')); // Output: "A-Bb-Ccc-Dddd"
 console.log(accum('RqaEzty')); // Output: "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 console.log(accum('cwAt')); // Output: "C-Ww-Aaa-Tttt"
+
 //---------------------------------------------------------------------------------------------------------------
