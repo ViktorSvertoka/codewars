f0108cb4d633c9fd579da406c0e8ae388c13cbd0
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-06 02:13:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -984,3 +984,47 @@ function alphabetPosition(text) {
 }
 
 //---------------------------------------------------------------------------------------------------------------
+
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+function isValidWalk(walk) {
+  // Check if the walk duration is exactly ten minutes
+  if (walk.length !== 10) {
+    return false;
+  }
+
+  // Count the number of steps in each direction
+  let count = {
+    n: 0,
+    s: 0,
+    e: 0,
+    w: 0,
+  };
+
+  for (let direction of walk) {
+    count[direction]++;
+  }
+
+  // Check if the north and south steps cancel each other out
+  if (count['n'] !== count['s']) {
+    return false;
+  }
+
+  // Check if the east and west steps cancel each other out
+  if (count['e'] !== count['w']) {
+    return false;
+  }
+
+  // If all conditions are met, the walk is valid
+  return true;
+}
+
+console.log(isValidWalk(['n', 's', 'w', 'e'])); // false
+console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])); // true
+console.log(isValidWalk(['n', 'n', 'n', 's', 's', 's', 'e', 'w', 'e', 'w'])); // true
+console.log(isValidWalk(['n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's'])); // true
+console.log(isValidWalk(['n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 'w'])); // false
+
+//---------------------------------------------------------------------------------------------------------------
