040474e41ffbb167bd8d16f9b85930fef886c428
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-05 22:58:25 +0300

    Feat(JS) Added script
    Task
    
    Co-Authored-By: Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>

--- script.js
+++ script.js
@@ -932,3 +932,22 @@ function findOdd(A) {
 console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1])); // Output: 4
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+//---------------------------------------------------------------------------------------------------------------
