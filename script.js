f434ab61976b79821d642a28fdfb6592cf7437d7
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-05 22:48:01 +0300

    Feat(JS) Added script
    Co-Authored-By: Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
    Co-Authored-By: Evan Svertoka <131455378+EvanSvertoka@users.noreply.github.com>

--- script.js
+++ script.js
@@ -895,3 +895,40 @@ function firstNonConsecutive(arr) {
 }
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+  let count = {};
+
+  // Count the occurrences of each number in the array
+  for (let i = 0; i < A.length; i++) {
+    if (count[A[i]] === undefined) {
+      count[A[i]] = 1;
+    } else {
+      count[A[i]]++;
+    }
+  }
+
+  // Find the number with an odd count
+  for (let num in count) {
+    if (count[num] % 2 !== 0) {
+      return parseInt(num);
+    }
+  }
+
+  return 0; // Default return if no number is found (should not happen based on the problem description)
+}
+
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1])); // Output: 4
+
+//---------------------------------------------------------------------------------------------------------------
