b08f90073ae65234a50b7cb676c7c960a24043f6
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-07 01:13:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1028,3 +1028,25 @@ console.log(isValidWalk(['n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's'])); //
 console.log(isValidWalk(['n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 'w'])); // false
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+function friend(friends) {
+  return friends.filter(name => name.length === 4);
+}
+
+// Example usage:
+const names = ['Ryan', 'Kieran', 'Jason', 'Yous'];
+const filteredNames = friend(names);
+console.log(filteredNames);
+
+//---------------------------------------------------------------------------------------------------------------
