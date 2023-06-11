e5d891daeb65a5492b569a060bf8e51be743cd6b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-12 00:15:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1184,3 +1184,47 @@ console.log(rentalCarCost(4)); // Output: 160 ($20 discount)
 console.log(rentalCarCost(8)); // Output: 310 ($50 discount)
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+// Go challenge Build Tower Advanced once you have finished this :)
+
+function towerBuilder(nFloors) {
+  let tower = [];
+  let width = 2 * nFloors - 1;
+
+  for (let i = 0; i < nFloors; i++) {
+    let spaces = ' '.repeat(nFloors - i - 1);
+    let blocks = '*'.repeat(2 * i + 1);
+    let floor = spaces + blocks + spaces;
+    tower.push(floor);
+  }
+
+  return tower;
+}
+
+let tower3Floors = towerBuilder(3);
+console.log(tower3Floors);
+
+let tower6Floors = towerBuilder(6);
+console.log(tower6Floors);
+
+//---------------------------------------------------------------------------------------------------------------
