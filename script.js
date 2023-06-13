50c43d9be68eb5be48868cbb27c072ae7a6b58c3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-14 01:09:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1351,3 +1351,37 @@ const games = [
 console.log(points(games)); // Output: 16
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Enough is enough!
+// Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
+// He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+// Task
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+// For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+// With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+function deleteNth(arr, n) {
+  const count = new Map();
+  const result = [];
+
+  for (const num of arr) {
+    if (!count.has(num)) {
+      count.set(num, 1);
+      result.push(num);
+    } else if (count.get(num) < n) {
+      count.set(num, count.get(num) + 1);
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
+console.log(deleteNth([1, 2, 3, 1, 2, 1, 2, 3], 2));
+// Output: [1, 2, 3, 1, 2]
+
+console.log(deleteNth([20, 37, 20, 21], 1));
+// Output: [20, 37, 21]
+
+//---------------------------------------------------------------------------------------------------------------
