86a9b9bcc75671dc5289d36056e46fbb830f6e97
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-15 11:47:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1450,3 +1450,35 @@ console.log(findUniq([1, 1, 1, 2, 1, 1])); // Output: 2
 console.log(findUniq([0, 0, 0.55, 0, 0])); // Output: 0.55
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+function strCount(str, letter) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(strCount('Hello', 'o')); // Output: 1
+console.log(strCount('Hello', 'l')); // Output: 2
+console.log(strCount('', 'z')); // Output: 0
+
+//---------------------------------------------------------------------------------------------------------------
