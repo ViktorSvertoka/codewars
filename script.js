7cf58b7dc4af0df2025f7176181cdac6858b7150
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-16 00:34:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1500,3 +1500,38 @@ function disemvowel(str) {
 console.log(disemvowel('This website is for losers LOL!'));
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+function getDivisorsCnt(n) {
+  let count = 0;
+
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      count += 2; // Increment count for both divisors i and n/i
+    }
+  }
+
+  // If n is a perfect square, decrement count by 1 to avoid double-counting the square root
+  if (Math.sqrt(n) % 1 === 0) {
+    count--;
+  }
+
+  return count;
+}
+
+console.log(getDivisorsCnt(4)); // Output: 3
+console.log(getDivisorsCnt(5)); // Output: 2
+console.log(getDivisorsCnt(12)); // Output: 6
+console.log(getDivisorsCnt(30)); // Output: 8
+
+//---------------------------------------------------------------------------------------------------------------
