b64d85032ff282e84bb5f8b30dd849c3318adf0a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-19 00:27:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1779,3 +1779,25 @@ const stringToNumber = function (str) {
 };
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here
+
+function century(year) {
+  // Calculate the century by dividing the year by 100 and rounding up
+  const century = Math.ceil(year / 100);
+
+  return century;
+}
+
+//---------------------------------------------------------------------------------------------------------------
