e50c6a8f1d32bf4cbd79ea8c73fae43989333f88
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-19 00:25:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1760,3 +1760,22 @@ console.log(countSmileys([';D', ':-(', ':-)', ';~)'])); // Output: 3
 console.log(countSmileys([';]', ':[', ';*', ':$', ';-D'])); // Output: 1
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+const stringToNumber = function (str) {
+  return parseInt(str);
+};
+
+//---------------------------------------------------------------------------------------------------------------
