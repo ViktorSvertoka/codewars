f9cf18c01d18e1d60766b669e478933f7e4c36b5
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-25 00:45:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2071,3 +2071,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 };
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+  return 'Hello, ' + name;
+}
+
+console.log(sayHello('Mr. Spock'));
+
+//---------------------------------------------------------------------------------------------------------------
