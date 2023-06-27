d0671b00e98377c7757c4a1a798a2b9c06e1a841
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-28 01:25:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2166,3 +2166,23 @@ console.log(areYouPlayingBanjo('Robert')); // Output: Robert plays banjo
 console.log(areYouPlayingBanjo('Alice')); // Output: Alice does not play banjo
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
+  ) {
+    return true; // They are in love
+  } else {
+    return false; // They are not in love
+  }
+}
+
+console.log(lovefunc(3, 4)); // Output: true
+console.log(lovefunc(2, 2)); // Output: false
+
+//---------------------------------------------------------------------------------------------------------------
