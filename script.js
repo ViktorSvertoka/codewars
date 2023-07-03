ff22cdafb472f81ce9e75d704c85e106cb68a63e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-04 00:06:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2356,3 +2356,29 @@ function evenOrOdd(number) {
 }
 
 //---------------------------------------------------------------------------------------------------------------
+
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+// maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// // should be 6: [4, -1, 2, 1]
+// Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+// Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+let maxSequence = function (arr) {
+  if (arr.length === 0) {
+    return 0; // Empty list, return 0
+  }
+
+  let maxSum = arr[0]; // Initialize maxSum with the first element
+  let currentSum = arr[0]; // Initialize currentSum with the first element
+
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return Math.max(maxSum, 0); // Return the maximum sum or 0 if all elements are negative
+};
+
+//---------------------------------------------------------------------------------------------------------------
