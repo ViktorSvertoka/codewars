c90a1d2ea3af99600237c80c49017abed4f71294
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-05 01:02:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2419,3 +2419,37 @@ function findAverage(array) {
 
   return sum / array.length; // Divide the sum by the count to get the average
 }
+
+//---------------------------------------------------------------------------------------------------------------
+
+// Your task is to write a function which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+// This is the first kata in the series:
+
+// Sum of a sequence (this kata)
+// Sum of a Sequence [Hard-Core Version]
+
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) {
+    return 0; // Return 0 if begin is greater than end
+  }
+
+  let sum = 0;
+  for (let i = begin; i <= end; i += step) {
+    sum += i; // Accumulate the sum of the numbers in the sequence
+  }
+
+  return sum;
+};
+
+//---------------------------------------------------------------------------------------------------------------
