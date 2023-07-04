e67fdd0dd799f346526374cfb5713dec005b2258
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-05 01:04:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2453,3 +2453,20 @@ const sequenceSum = (begin, end, step) => {
 };
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums) {
+  if (!nums || nums.length === 0) {
+    return []; // Return an empty array for null, nil, or empty array
+  }
+
+  return nums.sort((a, b) => a - b); // Sort the array in ascending order
+}
+
+//---------------------------------------------------------------------------------------------------------------
