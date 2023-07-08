58ed8a2a77b2b1a2fff6d9cb8d29755065b1debf
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-09 00:16:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2731,3 +2731,15 @@ console.log(highAndLow('1 2 -3 4 5')); // Output: "5 -3"
 console.log(highAndLow('1 9 3 4 -5')); // Output: "9 -5"
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+function addFive(num) {
+  let total = num + 5;
+  return total;
+}
+
+//---------------------------------------------------------------------------------------------------------------
