c365fa960ff7d03449f4c6bce9e712f85c2b2e40
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-13 01:09:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -172,3 +172,51 @@ console.log(checkForFactor(6, 3)); // Output: true
 console.log(checkForFactor(7, 2)); // Output: false
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let currentAttacker = firstAttacker;
+
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    if (currentAttacker === fighter1.name) {
+      fighter2.health -= fighter1.damagePerAttack;
+      currentAttacker = fighter2.name;
+    } else {
+      fighter1.health -= fighter2.damagePerAttack;
+      currentAttacker = fighter1.name;
+    }
+  }
+
+  if (fighter1.health <= 0) {
+    return fighter2.name;
+  } else {
+    return fighter1.name;
+  }
+}
+
+const fighter1 = new Fighter('Lew', 10, 2);
+const fighter2 = new Fighter('Harry', 5, 4);
+const firstAttacker = 'Lew';
+
+const winner = declareWinner(fighter1, fighter2, firstAttacker);
+console.log(winner); // Output: "Lew"
+
+//---------------------------------------------------------------------------------------------------------------
