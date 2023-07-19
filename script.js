4584987cb451efc51659426ed193102c33572f77
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-20 02:41:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -528,3 +528,30 @@ console.log(six(dividedBy(two()))); // Output: 3
 console.log(eight(dividedBy(three()))); // Output: 2
 
 //---------------------------------------------------------------------------------------------------------------
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme(triplet) {
+  // Find the middle number among the three elements
+  const middleNumber = triplet.sort((a, b) => a - b)[1];
+
+  // Return the index of the middle number in the original array
+  return triplet.indexOf(middleNumber);
+}
+
+console.log(gimme([2, 3, 1])); // Output: 0
+console.log(gimme([5, 10, 14])); // Output: 1
+
+//---------------------------------------------------------------------------------------------------------------
