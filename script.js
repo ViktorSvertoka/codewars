c16c53bdc9012ba23fa95104edcf0ad1b4e1f7d1
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-22 19:29:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -599,3 +599,15 @@ console.log(reverse('Hello World')); // Output: "World Hello"
 console.log(reverse('Hi There.')); // Output: "There. Hi"
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  if (typeof x === 'string') {
+    return 'Error';
+  } else {
+    return x * 50 + 6;
+  }
+}
+
+//---------------------------------------------------------------------------------------------------------------
