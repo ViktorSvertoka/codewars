f059a76055bf206d0759a4e25335c36049f782a5
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-23 03:19:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -611,3 +611,44 @@ function problem(x) {
 }
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+function peopleWithAgeDrink(age) {
+  if (age < 14) {
+    return 'drink toddy';
+  } else if (age < 18) {
+    return 'drink coke';
+  } else if (age < 21) {
+    return 'drink beer';
+  } else {
+    return 'drink whisky';
+  }
+}
+
+// Test cases
+console.log(peopleWithAgeDrink(13)); // Output: "drink toddy"
+console.log(peopleWithAgeDrink(17)); // Output: "drink coke"
+console.log(peopleWithAgeDrink(18)); // Output: "drink beer"
+console.log(peopleWithAgeDrink(20)); // Output: "drink beer"
+console.log(peopleWithAgeDrink(30)); // Output: "drink whisky"
+
+//---------------------------------------------------------------------------------------------------------------
