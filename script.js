5361629a13b892f47327bee32630057d2bc18a31
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-31 00:21:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -772,3 +772,34 @@ const b3 = [121, 14641, 20736, 36100, 25921, 361, 20736, 361];
 console.log(comp(a3, b3)); // Output: false
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+  return sum.toFixed(2);
+}
+
+// Test cases
+console.log(SeriesSum(1)); // Output: "1.00"
+console.log(SeriesSum(2)); // Output: "1.25"
+console.log(SeriesSum(5)); // Output: "1.57"
+
+//---------------------------------------------------------------------------------------------------------------
