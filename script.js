04f5100afa5e37138c32daeabdc60e4cd65ada33
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-07-30 22:26:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -705,3 +705,70 @@ console.log(hexToDec('FF')); // Output: 255
 console.log(hexToDec('10')); // Output: 16
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+// Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+// Note for C
+// The two arrays have the same size (> 0) given as parameter in function comp.
+
+function comp(array1, array2) {
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    return false;
+  }
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  // Sort both arrays in ascending order
+  const sortedArray1 = array1.sort((a, b) => a - b);
+  const sortedArray2 = array2.sort((a, b) => a - b);
+
+  // Compare each element
+  for (let i = 0; i < sortedArray1.length; i++) {
+    if (sortedArray1[i] * sortedArray1[i] !== sortedArray2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test cases
+const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+const b1 = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+console.log(comp(a1, b1)); // Output: true
+
+const a2 = [121, 144, 19, 161, 19, 144, 19, 11];
+const b2 = [132, 14641, 20736, 361, 25921, 361, 20736, 361];
+console.log(comp(a2, b2)); // Output: false
+
+const a3 = [121, 144, 19, 161, 19, 144, 19, 11];
+const b3 = [121, 14641, 20736, 36100, 25921, 361, 20736, 361];
+console.log(comp(a3, b3)); // Output: false
+
+//---------------------------------------------------------------------------------------------------------------
