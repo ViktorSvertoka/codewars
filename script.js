28507e161a6f2a263b12653fcc81cd3a08f3df35
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-03 00:38:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -851,3 +851,35 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 }
 
 //---------------------------------------------------------------------------------------------------------------
+
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+// Happy coding!
+
+function mergeArrays(arr1, arr2) {
+  // Merge both arrays and remove duplicates using a Set
+  const mergedSet = new Set([...arr1, ...arr2]);
+
+  // Convert the Set back to an array and sort it in ascending order
+  const mergedArray = Array.from(mergedSet).sort((a, b) => a - b);
+
+  return mergedArray;
+}
+
+// Test cases
+console.log(mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2])); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12])); // Output: [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+//---------------------------------------------------------------------------------------------------------------
