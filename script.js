d4ea8f906dc671307f7258659e21ddea32809bdb
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-09 01:08:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -919,3 +919,25 @@ console.log(doubleChar('Hello World')); // Output: "HHeelllloo  WWoorrlldd"
 console.log(doubleChar('1234!_ ')); // Output: "11223344!!__  "
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+function powersOfTwo(n) {
+  const result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+// Examples:
+console.log(powersOfTwo(0)); // Output: [1]
+console.log(powersOfTwo(1)); // Output: [1, 2]
+console.log(powersOfTwo(2)); // Output: [1, 2, 4]
+
+//---------------------------------------------------------------------------------------------------------------
