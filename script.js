f6c85864ac561de13697a8cf362731b3be4f8803
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-15 00:14:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,42 @@ console.log(getGrade(75, 80, 78)); // Output: 'C'
 console.log(getGrade(55, 45, 50)); // Output: 'F'
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+function oddOrEven(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  if (sum % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+}
+
+// Test cases
+console.log(oddOrEven([0])); // Output: "even"
+console.log(oddOrEven([0, 1, 4])); // Output: "odd"
+console.log(oddOrEven([0, -1, -5])); // Output: "even"
+
+//---------------------------------------------------------------------------------------------------------------
