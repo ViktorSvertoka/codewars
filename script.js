20710e09362050254d5b69c27f031fa4ef1e6c6d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-09-09 20:58:53 +0300

    Feat(HTML) Change code

--- script.js
+++ script.js
@@ -62,3 +62,11 @@ const encryptedMessage = rot13('Hello, World!');
 console.log(encryptedMessage);
 
 //---------------------------------------------------------------------------------------------------------------
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+  return bool ? 'Yes' : 'No';
+}
+
+//---------------------------------------------------------------------------------------------------------------
