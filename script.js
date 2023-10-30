b8152ea1b590b118d86e142ddbc3a43ea6e47458
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-30 23:44:49 +0200

    Feat(HTML & JS) Added button

--- script.js
+++ script.js
@@ -603,3 +603,20 @@ const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 // clearTimeout(timerId);
 
 //---------------------------------------------------------------------------------------------------------------
+
+const startBtn = document.querySelector('.js-start');
+const stopBtn = document.querySelector('.js-stop');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
+
+//---------------------------------------------------------------------------------------------------------------
