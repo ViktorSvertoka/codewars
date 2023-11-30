b032f6b73e6a6493153a304b334fba473d559399
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-30 16:40:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -361,16 +361,27 @@
 
 //--------------------------------------------------------------------------------------------------------------
 
+// const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+
+// const totalWithdrawalsEuro = transactions
+//   .filter(trans => trans < 0)
+//   // .map((trans, index, array) => {
+//   //   return trans * 0.86;
+//   // })
+//   .map(trans => trans * 0.86)
+//   .reduce((acc, trans) => acc + trans, 0);
+
+// console.log(totalWithdrawalsEuro);
+
+//--------------------------------------------------------------------------------------------------------------
+
 const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 
-const totalWithdrawalsEuro = transactions
-  .filter(trans => trans < 0)
-  // .map((trans, index, array) => {
-  //   return trans * 0.86;
-  // })
-  .map(trans => trans * 0.86)
-  .reduce((acc, trans) => acc + trans, 0);
+const firstDeposit = transactions.find(trans => trans > 0);
 
-console.log(totalWithdrawalsEuro);
+const firstWithdrawal = transactions.find(trans => trans < 0);
+
+console.log(firstDeposit);
+console.log(firstWithdrawal);
 
 //--------------------------------------------------------------------------------------------------------------
