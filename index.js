d616b13edaba5e31ac73e9ae8b4a374f309b3ea1
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-12-21 23:06:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -452,3 +452,33 @@
 // console.log(Number.isFinite(111));
 
 //--------------------------------------------------------------------------------------------------------------
+
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice as strong as love :-)
+
+// Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+// The input will always be made of only lowercase letters and will never be empty.
+
+function wordsToMarks(string) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let sum = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    const position = alphabet.indexOf(char) + 1;
+    sum += position;
+  }
+
+  return sum;
+}
+
+console.log(wordsToMarks('love')); // Output: 54
+console.log(wordsToMarks('friendship')); // Output: 108
+
+//--------------------------------------------------------------------------------------------------------------
